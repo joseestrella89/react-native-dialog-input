f8b5ca8a45a8a1e477422f3367dff4ee3a8ab882
Mike Dobrowolski <mdobro@umich.edu>
2018-11-26 14:37:47 -0500

    ensures background fills screen on Android

--- index.js
+++ index.js
@@ -81,6 +81,8 @@ class DialogInput extends React.Component{
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    width: '100%',
+    height: '100%',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
